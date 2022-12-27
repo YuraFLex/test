3e40fc7e03f5fb214090cd8436bb119a1d74f85d
YuraFLex <shvalov.yurec@gmail.com>
2022-12-27 10:25:48 +0200

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { HeroSlider } from "./HeroSlider/HeroSlider";
+
+
+export const App = () => {
+  return (
+   <>
+   <HeroSlider/>
+   </>
+  );
+};
---
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { HeroSliderData } from './HeroSliderData';
+
+import './HeroSlider.scss';
+
+export const HeroSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = HeroSliderData.length;
+
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide]);
+
+  return (
+    <div className="slider">
+      <AiOutlineArrowLeft onClick={prevSlide} className="arrow prev" />
+      <AiOutlineArrowRight onClick={nextSlide} className="arrow next" />
+      {HeroSliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === currentSlide ? 'slide current' : 'slide'}
+            key={index}
+          >
+            {index === currentSlide && (
+              <>
+                <img src={slide.img} alt="slide" />
+                <div className="content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.description}</p>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
